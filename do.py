c2143adf608b7a660a1525c90dcd3280935c8467
David Cottrell <cottrell@users.noreply.github.com>
2017-07-24 21:37:04 +0100

    update

--- do.py
+++ do.py
@@ -1,13 +1,23 @@
 import glob
+import multiprocessing
 import os
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+def _makepng(f):
+    r = svg2rlg(f)
+    ff = os.path.join('png', os.path.basename(f.replace('.svg', '.png')))
+    print('writing {}->{}'.format(f, ff))
+    renderPM.drawToFile(r, ff)
+
 def makepng():
     files = glob.glob('svgs/*.svg')
     if not os.path.exists('png'):
         os.makedirs('png')
+    pool = multiprocessing.Pool()
+    res = list()
     for i, f in enumerate(files):
-        r = svg2rlg(f)
-        ff = os.path.join('png', os.path.basename(f.replace('.svg', '.png')))
-        print(i, f, ff)
-        renderPM.drawToFile(r, ff)
+        res.append(pool.apply_async(_makepng, (f,)))
+    pool.close()
+    pool.join()
+    for x in res:
+        x.get()
