469d5f9d9c0f3f00a0f3a1bc7c38c4bbb7f84cf9
David Cottrell <cottrell@users.noreply.github.com>
2017-07-25 21:55:00 +0100

    add make array

--- do.py
+++ do.py
@@ -1,8 +1,10 @@
 import glob
+import numpy as np
 import multiprocessing
 import os
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+import PIL
 def _makepng(f):
     r = svg2rlg(f)
     ff = os.path.join('png', os.path.basename(f.replace('.svg', '.png')))
@@ -21,3 +23,24 @@ def makepng():
     pool.join()
     for x in res:
         x.get()
+
+def _makearray(f, newsize=32):
+    print(f)
+    im = PIL.Image.open(f)
+    im = im.resize((newsize, newsize))
+    return np.fromstring(im.tobytes(), dtype=np.uint8)
+
+def make_np_data(newsize=32):
+    files = glob.glob('png/*.png')
+    pool = multiprocessing.Pool()
+    res = list()
+    for i, f in enumerate(files):
+        res.append(pool.apply_async(_makearray, (f,), dict(newsize=newsize)))
+    pool.close()
+    pool.join()
+    m = len(files)
+    n = newsize ** 2
+    a = np.empty((m, n), dtype=np.uint8)
+    for x in res:
+        a[i,:] = x.get()
+    return a
